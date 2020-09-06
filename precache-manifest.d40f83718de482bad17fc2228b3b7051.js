6eea07cc9c07ee132beb8a3f76499e995c817dbb
Сергей Харлов <l1nks@inbox.ru>
2020-09-06 18:16:39 +0500

    Updates

---
+++ precache-manifest.d40f83718de482bad17fc2228b3b7051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256038fe1d95b3c23475c98a27a55a6c",
+    "url": "//ecwid-test/index.html"
+  },
+  {
+    "revision": "9911b8f7c519724aa310",
+    "url": "//ecwid-test/static/css/main.3397f376.chunk.css"
+  },
+  {
+    "revision": "f09062182b8359c2b5b0",
+    "url": "//ecwid-test/static/js/2.5d333ea8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//ecwid-test/static/js/2.5d333ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9911b8f7c519724aa310",
+    "url": "//ecwid-test/static/js/main.1f0e98af.chunk.js"
+  },
+  {
+    "revision": "94eeeeb7929e3dfccc23",
+    "url": "//ecwid-test/static/js/runtime-main.2968f9ea.js"
+  }
+]);
