f1fd8beea2062c538e606f73ba5cf77c9eebcaa9
Сергей Харлов <l1nks@inbox.ru>
2020-09-16 00:25:09 +0500

    перенес высоту в константу + рефакторинг кода

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './style/style.scss';
 import PropTypes from 'prop-types';
 import { handleUrl } from '../../utils/utils';
+import BASE_IMAGE_HEIGHT from '../../constants/constants';
 
 const Gallery = (props) => {
   const handleDrop = (evt) => {
@@ -21,14 +22,21 @@ const Gallery = (props) => {
   };
   const createImage = (image) => {
     const ratio = image.width / image.height;
-    const widthBase = ratio * 200;
+    const widthBase = ratio * BASE_IMAGE_HEIGHT;
 
     return (
       <div className="gallery__item" style={{ flexGrow: ratio }} key={image.id}>
         <span className="gallery__item-icon-close" onClick={() => props.deletePicture(image.id)}>
           <i className="fas fa-trash-alt" />
         </span>
-        <img className="gallery__item-img" width={widthBase} height={200} src={image.url} alt="Изображение" style={{ flexGrow: 1 }} />
+        <img
+          className="gallery__item-img"
+          width={widthBase}
+          height={BASE_IMAGE_HEIGHT}
+          src={image.url}
+          alt="Изображение"
+          style={{ flexGrow: 1 }}
+        />
       </div>
     );
   };
