f49e4ec9c3795963795cf94adef853ca8a40cd96
Сергей Харлов <l1nks@inbox.ru>
2020-09-16 00:12:07 +0500

    фикс названия класса для спинерра

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -3,8 +3,8 @@ import './style/style.scss';
 
 const Loader = () => (
   <div className="spinner">
-    <svg className="spinner__circle" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle className="spinner__path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30" />
+    <svg className="spinner__svg-icon" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+      <circle className="spinner__svg-path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30" />
     </svg>
   </div>
 );
